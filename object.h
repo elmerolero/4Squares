9f8c3dc1fd30a9bedbc5caebd93a56d97f0ffebd
elmerolero <isma.salas@outlook.com>
2020-04-05 17:08:05 -0500

    Object class

---
+++ object.h
@@ -0,0 +1,53 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+#include <SDL2/SDL.h>
+#include "game.h"
+
+class Object
+{
+	public:
+		Object();
+		~Object();
+		
+		// Load the relative coords from file
+		bool loadCoordinatesFromFile( const char * path );
+		
+		// When display is resized, updates the absolute coordinates
+		void updateAbsCoords( void );
+		
+		// Sets relative coords from given rect
+		void setRelativeCoords( SDL_DRect rect );
+		
+		// Sets texture coords from given rect
+		void setTextureCoords( SDL_Rect rect );
+		
+		// Sets the relative position of the object
+		void setRelativeX( double x );
+		void setRelativeY( double y );
+		
+		// Gets the relative position of the object
+		double getRelativeX();
+		double getRelativeY();
+		double getRelativeW();
+		double getRelativeH();
+		
+		// Gets
+		int getAbsoluteX();
+		int getAbsoluteY();
+		
+		void setAbsoluteX( int x );
+		void setAbsoluteY( int y );
+		
+		// Returns the src rect
+		SDL_Rect* getSrcRect( void );
+		
+		// Returns the dest rect 
+		SDL_Rect* getDestRect( void );
+		
+	private:
+		SDL_Rect  textureRect;
+		SDL_DRect relativeRect;
+		SDL_Rect  absoluteRect;
+};
+
+#endif
